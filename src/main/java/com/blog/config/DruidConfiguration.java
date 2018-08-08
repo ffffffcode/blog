671465688913d76c4e988b152fd08cc9ab0a88a5
AaronFae <AaronFae@163.com>
2018-08-08 15:03:26 +0800

    集成Druid数据源与监控

--- src/main/java/com/blog/config/DruidConfiguration.java
+++ src/main/java/com/blog/config/DruidConfiguration.java
@@ -1,5 +1,78 @@
 package com.blog.config;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import com.blog.config.properties.DruidProperties;
+
+@Configuration
+@EnableConfigurationProperties(DruidProperties.class)
 public class DruidConfiguration {
 
+	@Autowired
+	private DruidProperties properties;
+
+	@Bean
+	public DataSource dataSource() {
+		DruidDataSource druidDataSource = new DruidDataSource();
+		druidDataSource.setDriverClassName(properties.getDriverClassName());
+		druidDataSource.setUrl(properties.getUrl());
+		druidDataSource.setUsername(properties.getUsername());
+		druidDataSource.setPassword(properties.getPassword());
+		try {
+			druidDataSource.init();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return druidDataSource;
+	}
+
+	/**
+	 * 注册Servlet信息， 配置监控视图
+	 *
+	 * @return
+	 */
+	@Bean
+	public ServletRegistrationBean druidServlet() {
+		ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),
+				"/druid/*");
+
+		// 白名单：
+		// servletRegistrationBean.addInitParameter("allow", "192.168.6.195");
+		// IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to
+		// view this page.
+		// servletRegistrationBean.addInitParameter("deny", "192.168.6.73");
+		// 登录查看信息的账号密码, 用于登录Druid监控后台
+		servletRegistrationBean.addInitParameter("loginUsername", "blog");
+		servletRegistrationBean.addInitParameter("loginPassword", "blog");
+		// 是否能够重置数据.
+		servletRegistrationBean.addInitParameter("resetEnable", "true");
+		return servletRegistrationBean;
+
+	}
+
+	/**
+	 * 注册Filter信息, 监控拦截器
+	 *
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean filterRegistrationBean() {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(new WebStatFilter());
+		filterRegistrationBean.addUrlPatterns("/*");
+		filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+		return filterRegistrationBean;
+	}
 }
