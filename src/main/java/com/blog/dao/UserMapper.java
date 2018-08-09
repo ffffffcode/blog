2bbee7928b6f3ed75312c47622a3d6eb3ad5d960
AaronFae <AaronFae@163.com>
2018-08-09 21:44:31 +0800

    update UserMapper.java

--- src/main/java/com/blog/dao/UserMapper.java
+++ src/main/java/com/blog/dao/UserMapper.java
@@ -1,6 +1,7 @@
 package com.blog.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
@@ -10,4 +11,6 @@ import com.blog.entity.User;
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
 
+	@Select("select id, username from t_user where id = #{id}")
+	User getUserByIdWithoutPassword(Integer id);
 }
